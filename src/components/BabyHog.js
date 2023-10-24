45389199573fe96c8e3b946773fc321bd3498b98
Gorgees Odisho <gorgees041@gmail.com>
2023-10-24 03:18:04 +0300

    ACTIVITY DONE

--- src/components/BabyHog.js
+++ src/components/BabyHog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import normalBaby from "../assets/unadulterated-hoglette.png";
 import SunBaby from "../assets/sun-eyes.png";
 import BlueBaby from "../assets/blue-eyes.png";
@@ -7,26 +7,48 @@ import GlowingBaby from "../assets/glowing-eyes.png";
 // is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
-function BabyHog(props) {
+function BabyHog({ name, hobby, eyeColor }) {
+  const [weight, setWeight] = useState(1);
   function handleChangeWeight(e) {
     // how can we reuse this for both buttons?
     // perhaps something with e.target.name === "+"
+    name = e.target.name;
+    if (name === "+") {
+      setWeight(Number(weight + 0.5));
+    } else if (name === "-" && weight > 0) {
+      setWeight(Number(weight - 0.5));
+    }
   }
+  console.log(eyeColor);
+
+  const hogBabyImg = () => {
+    if (eyeColor === "sun") {
+      return SunBaby;
+    } else if (eyeColor === "blue") {
+      return BlueBaby;
+    } else if (eyeColor === "glowing") {
+      return GlowingBaby;
+    }
+  };
 
   return (
     <li className="hogbabies">
-      <h1>Name</h1>
-      <h3>Weight:</h3>
-      <h3>Hobby:</h3>
-      <h4>Eye Color:</h4>
+      <h1>{name}</h1>
+      <h3>Weight: {weight} Tons</h3>
+      <h3>Hobby: {hobby}</h3>
+      <h4>Eye Color: {eyeColor}</h4>
 
-      <button name="+">Increase Weight</button>
-      <button name="-">Decrease Weight</button>
+      <button name="+" onClick={handleChangeWeight}>
+        Increase Weight
+      </button>
+      <button name="-" onClick={handleChangeWeight}>
+        Decrease Weight
+      </button>
 
       <div className="hb-wrap">
         <img
-          src={normalBaby}
-          style={{ height: "200px" }}
+          src={hogBabyImg()}
+          style={{ height: `${200 + weight * 10}px` }}
           alt="MasterBlasterJrJr"
         />
       </div>
